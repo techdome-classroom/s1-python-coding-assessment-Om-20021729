74d65d66584374e0af77c514304112e4105a07f4
Om-20021729 <omtripathi1729@gmail.com>
2024-10-07 13:08:50 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,5 +1,24 @@
-def decode_message( s: str, p: str) -> bool:
+def is_match(message, pattern):
+   
+    if len(pattern) == 0:
+        return len(message) == 0
 
-# write your code here
-  
-        return False
+   
+    if pattern[0] == '*':
+       
+        return is_match(message, pattern[1:]) or (len(message) > 0 and is_match(message[1:], pattern))
+
+   
+    if pattern[0] == '?' or pattern[0] == message[0]:
+        return is_match(message[1:], pattern[1:])
+
+   
+    return False
+
+
+print(is_match("aa", "a"))
+print(is_match("aa", "*"))     
+print(is_match("cb", "?a"))      
+print(is_match("abcd", "a*d"))  
+print(is_match("abcd", "?b*d")) 
+print(is_match("abcd", "?b?*"))  
